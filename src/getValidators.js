0f8413898b85d267f8a38441fdb91a93bae6ab9c
edoubart <edouard.barthelemy@gmail.com>
2025-02-24 12:27:01 +0100

    Get Validators.

---
+++ src/getValidators.js
@@ -0,0 +1,43 @@
+/*
+ * A validator is someone who validates transactions that occur on the Solana
+ * blockchain. Validators are responsible for protecting the blockchain's
+ * integrity from any fraudulent transactions.
+ */
+
+// NPM Packages
+import { clusterApiUrl, Connection } from '@solana/web3.js';
+
+// Constants
+const SOLANA_NETWORK = 'devnet';
+const SOLANA_PREFLIGHT_COMMITMENT = 'processed'; // 'finalized'
+
+// Solana
+const network = clusterApiUrl(SOLANA_NETWORK);
+const options = {
+  preflightCommitment: SOLANA_PREFLIGHT_COMMITMENT,
+};
+
+async function main() {
+  let connection = new Connection(network, options.preflightCommitment);
+
+  /*
+   * Current vote accounts are validators that are currently active whereas
+   * delinquent vote accounts are validators that are currently inactive.
+   */
+  let { current, delinquent } = await connection.getVoteAccounts();
+
+  console.log('currentCount: ', current.length);
+  console.log('currentFirst: ', current[0]);
+  console.log('delinquentCount: ', delinquent.length);
+}
+
+async function runMain() {
+  try {
+    await main();
+  }
+  catch(error) {
+    console.log("Something went wrong fetching validators: ", error);
+  }
+}
+
+runMain();
